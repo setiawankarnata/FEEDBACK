e7d6df0ed0d3a4ace20ffa7238c90bc9f1e6879e
Setiawan Karnata <setiawankarnatachan@gmail.com>
2022-01-01 19:08:02 +0700

    First Initialize

---
+++ reviews/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from .models import Review
+
+
+# class ReviewForm(forms.Form):
+#     user_name = forms.CharField(label="Your name", max_length=20,
+#                                 error_messages={"required": "Your name must not be empty.",
+#                                                 "max_length": "Please enter a shorter name!"})
+#     review_text = forms.CharField(label="Your feedback", widget=forms.Textarea, max_length=200)
+#     rating = forms.IntegerField(min_value=1, max_value=5)
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = '__all__'
+        labels = {
+            'user_name': 'Your Name',
+            'review_text': 'Your Feedback',
+            'rating': 'Your Rating'
+        }
+        error_messages = {
+            "user_name": {
+                'required': 'Your name must not be empty!',
+                'max_length': 'Please enter a shorter name!'
+            }
+        }
